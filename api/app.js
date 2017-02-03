830150cf789c5c7afcdbf3f8730022108e938d1a
chao <dc10101@qq.com>
2017-02-03 11:31:49 +0800

    error handler

--- api/app.js
+++ api/app.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require('express')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-const routes = require('./routes');
+const routes = require('./routes')
 
-const app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', routes);
+app.use('/', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err) // will skip any non-error-handler middleware
+})
 
-// error handler
+// error-handler-middleware has err as the first parameter
 app.use(function(err, req, res, next) {
   let json = {} // ensure response in JSON format
 
-  if(err instanceof Error){
+  if(_.isError(err)){
     if(req.app.get('env') === 'development'){
       json.stack = err.stack // add stack in development environment
     }
     json.message = err.message
   }
-  else if(typeof(err) === 'string'){
+  else if(_.isString(err)){
     json.message = err // string as the error message
   }
-  else if(Object.prototype.toString.call(err) === '[object Object]'){
+  else if(_.isPlainObject(err)){
     json = err // JSON object as response
   }
 
   res.status(err.status || 500).json(json)
-});
+})
 
-module.exports = app;
+module.exports = app
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const vhost = require('vhost')
+global._ = require('lodash') // import lodash globally
 
 // main-app
 const app = express()
@@ -10,7 +11,15 @@ const admin = require('./admin/app')
 const h5 = require('./h5/app')
 
 app.use(vhost('admin.example.com', admin))
+
 app.use(vhost('api.example.com', api))
-app.use('/', h5)
+
+app.use(vhost('www.example.com', h5))
+app.use(vhost('example.com', h5))
+
+app.use(function (req, res) {
+  console.error('404 in main app')
+  res.status(404).send('Not Found')
+})
 
 module.exports = app
